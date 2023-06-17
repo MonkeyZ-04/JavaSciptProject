c49eea3e5dc6a3032a581cbd4100b5a43ba1c8bc
MonkeyZ-04 <89679927+MonkeyZ-04@users.noreply.github.com>
2023-06-17 22:15:27 +0700

    Update/html, javascript

--- Tracking Income & Expenses Program/script.js
+++ Tracking Income & Expenses Program/script.js
@@ -10,21 +10,64 @@ const amount = document.getElementById('amount');
 const dataTransaction=[
     {id:1,text:"ค่าขนม",amount:-100},
     {id:2,text:"ค่าห้อง",amount:-3000},
-    {id:3,text:"เงินเดือน",amount:+18000}
+    {id:3,text:"เงินเดือน",amount:+18000},
+    {id:4,text:"ค่าอาหาร",amount:-500},
+    {id:5,text:"ค่าหวย",amount:+20000}
 ]
 
 const transactions=dataTransaction;
 
 function init(){
     transactions.forEach(addDataToList);
+    calculateMoney()
 }
 function addDataToList(transactions){
     const symbol = transactions.amount < 0 ?'-':'+';
     const status = transactions.amount < 0 ?'minus':'plus';
     const item = document.createElement('li')
+    result = formatNumber(Math.abs(transactions.amount))
     item.classList.add(status)
     // item.innerHTML ='ค่าซ่อมรถ <span>- ฿400</span><button class="delete-btn">x</button>'
-    item.innerHTML =`${transactions.text}<span>${symbol}${Math.abs(transactions.amount)}</span><button class="delete-btn">x</button>`;
+    item.innerHTML =`${transactions.text}<span>${symbol}${result}</span><button class="delete-btn">x</button>`;
     list.appendChild(item)
 }
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+function autoID(){
+    return Math.floor(Math.random()*1000000)
+}
+function calculateMoney(){
+    const amounts=transactions.map(transactions=>transactions.amount);
+    // คำนวนยอดคงเหลือ
+    const total=amounts.reduce((result,item)=>(result+=item),0).toFixed(2);
+    // คำนวนรายรับ
+    const income = amounts.filter(item=>item>0).reduce((result,item)=>(result+=item),0).toFixed(2);
+    // คำนวนรายจ่าย 
+    const expense = (amounts.filter(item=>item<0).reduce((result,item)=>(result+=item),0)*-1).toFixed(2);
+
+    //Show on Display
+    balance.innerText=`฿${formatNumber(total)}`
+    money_plus.innerText=`฿${formatNumber(income)}`
+    money_minus.innerText=`฿`+formatNumber(expense);
+}
+function addTransaction(e){
+    e.preventDefault();
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        alert("Pls Fill The Box!");
+    }else{
+        const data={
+            id:autoID(),
+            text:text.value,
+            amount:+amount.value
+        }
+        transactions.push(data);
+        addDataToList(data);
+        calculateMoney;
+        text.value='';
+        amount.value='';
+    }
+}
+
+form.addEventListener('submit',addTransaction);
 init();

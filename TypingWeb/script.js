f3625157f779338cc2a4cfeee271f545301f56d7
MonkeyZ-04 <89679927+MonkeyZ-04@users.noreply.github.com>
2023-07-03 22:28:43 +0700

    update

---
+++ TypingWeb/script.js
@@ -0,0 +1,10 @@
+const wordEl=document.getElementById('word');
+const textEl=document.getElementById('text');
+const scoreEl=document.getElementById('score');
+const timeEl=document.getElementById('time');
+
+const btnLevelEl=document.getElementById('level-btn');
+const settingsEl=document.getElementById('settings');
+const levelFormEl=document.getElementById('level-form');
+const levelEl=document.getElementById('level');
+const gameoverEl=document.getElementById('gameover-container');

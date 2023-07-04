6b3fbbd71b3ea6f8015f7b17bb35c70ecc89b4c0
MonkeyZ-04 <89679927+MonkeyZ-04@users.noreply.github.com>
2023-07-04 21:56:15 +0700

    Update/ script.js

--- TypingWeb/script.js
+++ TypingWeb/script.js
@@ -8,3 +8,15 @@ const settingsEl=document.getElementById('settings');
 const levelFormEl=document.getElementById('level-form');
 const levelEl=document.getElementById('level');
 const gameoverEl=document.getElementById('gameover-container');
+
+const words=["เศรษฐี","แมว","หมู"];
+// words = เก็บคำ wordEl = แสดงผล
+
+let randomWord;
+let score=0;
+let time=15;//easy => 15 , medium => 10,hard => 5
+
+function getRamdomWord(){
+    return words[Math.floor(Math.random()*words.length)]
+    
+}
